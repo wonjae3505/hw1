d738ebb419eea7f98bb659174d9cfe215fd5a71b
wonjae3505 <69029830+wonjae3505@users.noreply.github.com>
2020-08-01 09:23:12 +0900

    Create 안녕하세요.py
    Hello

---
+++ 안녕하세요.py
@@ -0,0 +1,23 @@
+def fib(n):
+    a, b= 0,1
+    while a < n:
+        print(a,end=' ')
+        a,b = b, a+b
+    print()
+
+def fib2(n):
+    result = []
+    a, b= 0,1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+
+import sys
+
+if __name__=="__main__":
+    import sys 
+    fib(int(sys.argv[1]))
+else: 
+    print("메인이 아닙니다")
+
